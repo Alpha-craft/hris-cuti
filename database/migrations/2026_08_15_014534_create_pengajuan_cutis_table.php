7cf4a90964658a114f6baec4180299aa32ed311d
Alpha-Craft <ainurahman03@gmail.com>
2026-08-15 09:04:07 +0700

    add: migrations for pengajuan cuti

--- database/migrations/2026_08_15_014534_create_pengajuan_cutis_table.php
+++ database/migrations/2026_08_15_014534_create_pengajuan_cutis_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('pengajuan_cutis', function (Blueprint $table) {
             $table->id();
+            $table->foreign('karyawan_id')->references('id')->on('karyawans');
+            $table->date('tanggal_pengajuan');
+            $table->text('alasan');
+            $table->boolean('status_approval');
             $table->timestamps();
         });
     }
