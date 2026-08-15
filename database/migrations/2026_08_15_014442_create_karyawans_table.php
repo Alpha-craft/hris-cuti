329c5ae3e2a4ad65bce30a3defefb68eb88abbad
Alpha-Craft <ainurahman03@gmail.com>
2026-08-15 08:54:17 +0700

    add: migration and model for karyawan

---
+++ database/migrations/2026_08_15_014442_create_karyawans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap');
+            $table->integer('status_jabatan');
+            $table->integer('id_manajer');
+            $table->integer('sisa_kuota_cuti');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawans');
+    }
+};
