52d2c09c97c4eaee02f7c1f3e582c9986dc0dc28
Alpha-Craft <ainurahman03@gmail.com>
2026-08-15 16:32:07 +0700

    fix: constraint error on table pengajuan WFH

--- database/migrations/2026_08_15_014558_create_pengajuan_wfhs_table.php
+++ database/migrations/2026_08_15_014558_create_pengajuan_wfhs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('pengajuan_wfhs', function (Blueprint $table) {
             $table->id();
-            $table->foreign('id_karyawan')->references('id')->on('karyawans');
+            $table->foreignId('id_karyawan')->references('id')->on('karyawans');
             $table->string('koordinat_gps');
             $table->string('foto_selfie');
             $table->text('activity_log');
