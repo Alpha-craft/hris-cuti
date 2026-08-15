8bef04b51ee2195d9c4faf88eab9d399bb781bc1
Alpha-Craft <ainurahman03@gmail.com>
2026-08-15 19:53:23 +0700

    add: relationship

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -3,9 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Karyawan extends Model
 {
     protected $guarded = [];
-    //
+
+    public function pengajuanCutis():HasMany {
+        return $this->hasMany(PengajuanCuti::class);
+    }
+
+    public function pengajuanLembur():HasMany {
+        return $this->hasMany(PengajuanLembur::class);
+    }
+
+    public function pengajuanWfh():HasMany {
+        return $this->hasMany(PengajuanWfh::class);
+    } 
+
 }
